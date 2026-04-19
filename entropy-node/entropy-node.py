db88cfa040511f4d859f21f046811c02bf072327
Mert Cobanov <mertcobanov@gmail.com>
2026-04-19 09:12:55 -0700

    Add solution for entropy-node

---
+++ entropy-node/entropy-node.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def entropy_node(y):
+    """
+    Compute entropy for a single node using stable logarithms.
+    """
+    # Write code here
+    _, counts = np.unique(y, return_counts=True)
+    probs = counts / counts.sum()
+    probs = probs[probs > 0]
+    return -np.sum(probs * np.log2(probs)).item()
