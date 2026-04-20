6625401f022fc3994245959f0161f968882514f4
Mert Cobanov <mertcobanov@gmail.com>
2026-04-20 06:38:52 -0700

    Update solution for pandas-read-csv

--- study-plans/pandas-basics/pandas-read-csv/pandas-read-csv.py
+++ study-plans/pandas-basics/pandas-read-csv/pandas-read-csv.py
@@ -4,14 +4,10 @@ def create_dataframe(data):
     """
     Returns: dict with 'data', 'shape', 'columns'
     """
+    
     df = pd.DataFrame.from_dict(data)
-
     return {
-
         "data": df.to_dict(orient="list"),
-
         "shape": list(df.shape),
-
         "columns": df.columns.to_list()
-
     }
